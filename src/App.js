9888451e5355b9bd725e803d4073d8e694d08c91
seongjin77 <kssj9056@gmail.com>
2022-05-22 01:21:53 +0900

    셍활코딩 리액트

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(){
+function Header(props){
+  
+  console.log(props,props.title);
     return  <header>
-            <h1><a href="/">WEB</a></h1>
+            <h1><a href="/">{props.title}</a></h1>
             </header>
 }
-
+/* 반드시 대문자로 만들어야함 */
 /* 사용자 정의 함수를 컴퍼넌트라고 부른다 */
-function Nav(){
+function Nav(props){
+    const lis = [
+      
+    ]
+    for (let i=0; i<props.topics.length; i++){
+        let t = props.topics[i];
+        lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+    }
     return <nav>
        <ol>
-         <li><a href="/">html</a></li>
-         <li><a href="/">css</a></li>
-         <li><a href="/">js</a></li>
-         <li><a href="/">react</a></li>
+          {lis}
        </ol>
             </nav>
 
 }
-function Article(){
+function Article(props){
   return  <article>
-          <h2>Welcome</h2>
-          Hello, WEB....
+          <h2>{props.title}</h2>
+          {props.body}
            </article>
 }
 function App() {
+
+  const topics = [
+        {id: 1, title: 'html', body: 'html is ...'},
+        {id: 2, title: 'css', body: 'css is ...'},
+        {id: 3, title: 'javascript', body: 'javascript is ...'}
+  ];
   return (
     <div>
-     <Header></Header>
-     <Header></Header>
-     <Header></Header>
-      <Nav></Nav>
-      <Nav></Nav>
-      <Article></Article>
+     <Header title='REACT'></Header>
+     <Header title='REACT'></Header>
+      <Nav topics={topics}></Nav>
+      <Article title='welcome' body='hello, web'></Article>
     </div>
   );
 }
