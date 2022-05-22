8741770b7a6003a367f845b302335cc062d668b8
seongjin77 <kssj9056@gmail.com>
2022-05-22 15:01:27 +0900

    생활코딩 react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(props){
-  
-  console.log(props,props.title);
-    return  <header>
-            <h1><a href="/">{props.title}</a></h1>
-            </header>
-}
 /* 반드시 대문자로 만들어야함 */
 /* 사용자 정의 함수를 컴퍼넌트라고 부른다 */
+
+function Article(props){
+  return  <article>
+          <h2>{props.title}</h2>
+          {props.body}
+           </article>
+}
+function Header(props){
+  
+  // console.log(props,props.title);
+    return  <header>
+            <h1><a href="/" onClick={(event)=>{
+                event.preventDefault(); // a태그 기본 동작 방지, 클릭해도 페이지 넘김x
+                props.onchangeMode();
+            }}>{props.title}</a></h1>
+            </header>
+}
 function Nav(props){
-    const lis = [
-      
-    ]
-    for (let i=0; i<props.topics.length; i++){
+  const lis = []
+  for (let i=0; i<props.topics.length; i++){
         let t = props.topics[i];
-        lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+        lis.push(<li key={t.id}>
+          <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
+            event.preventDefault();
+            props.onchangeMode(event.target.id);
+          }}>{t.title}</a></li>);
     }
     return <nav>
        <ol>
@@ -23,13 +35,7 @@ function Nav(props){
        </ol>
             </nav>
 
-}
-function Article(props){
-  return  <article>
-          <h2>{props.title}</h2>
-          {props.body}
-           </article>
-}
+}/// Nav 함수 끝
 function App() {
 
   const topics = [
@@ -39,9 +45,12 @@ function App() {
   ];
   return (
     <div>
-     <Header title='REACT'></Header>
-     <Header title='REACT'></Header>
-      <Nav topics={topics}></Nav>
+     <Header title='REACT' onchangeMode={()=>{
+       alert('Header');
+     }}></Header>
+      <Nav topics={topics} onchangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title='welcome' body='hello, web'></Article>
     </div>
   );
